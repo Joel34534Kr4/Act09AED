d732ac9bc8a0d476bbbbd4b1eb2a3a2913773cf2
Joel_UCSM <joel.ventura@estudiante.ucsm.edu.pe>
2025-06-08 20:44:52 -0500

    Update Vertex.java

--- graph/Vertex.java
+++ graph/Vertex.java
@@ -1,19 +1,19 @@
 package graph;
+package graph;
 import ListLinked.ListLinked;
-
+// clase generica que representa un vertice en el grafo
 public class Vertex<E> {
-    private E data;
-    protected ListLinked<Edge<E>> listAdj;
-
-    public Vertex(E data) {
+    private E data; // dato almacenado en el vertice
+    protected ListLinked<Edge<E>> listAdj; //listas de aristas adyacentes
+    public Vertex(E data) { //metodo constructor que inicializa el vertice con un dato y una lista vacia de aristas
         this.data = data;
         listAdj = new ListLinked<>();
     }
 
-    public E getData() {
+    public E getData() { //un getter para obtener el dato del vertice
         return data;
     }
-
+    //Método equals que compara dos vértices basándose en el dato.
     public boolean equals(Object o) {
         if (o instanceof Vertex<?>) {
             Vertex<E> v = (Vertex<E>) o;
@@ -21,6 +21,11 @@ public class Vertex<E> {
         }
         return false;
     }
+    //Representación en cadena del vértice y sus aristas adyacentes.
+    public String toString() {
+        return this.data + " --> " + this.listAdj.toString() + "\n";
+    }
+}
 
     public String toString() {
         return this.data + " --> " + this.listAdj.toString() + "\n";
